83d074947864b7ee582f3a3b9e9f8ed5995e4cdd
Nguyen <dnguyen1240@student.cccd.edu>
2023-12-07 19:09:19 -0800

    added danvnguyen folder and files

---
+++ danvnguyen/awesome.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This is awesome!" << endl;
+	cout << "Something new" << endl;
+	int x = 5;
+	int y = x * 6;
+	x = y;
+	cout << "x = " << x;
+	return 0;
+}
