ea8d26cbec7d21fb7af81fdf44d2148e13e3309d
Tran <ktran770@student.cccd.edu>
2023-12-07 19:09:28 -0800

    added cs-kyletran folder and files

---
+++ cs-kyletran/awesome.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+     cout << "This is awesome" << endl;
+     int number = 7;
+     cout << number << endl;
+     return 0;
+}
